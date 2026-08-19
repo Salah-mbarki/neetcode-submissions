ba858e734d0a0913ca94dbaa0645b6c6d7965435
Salah-mbarki <145559443+Salah-mbarki@users.noreply.github.com>
2026-08-19 14:22:02 +0100

    Add: is-palindrome - submission-19

---
+++ Data Structures & Algorithms/is-palindrome/submission-19.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        int l = 0 , r = s.Length-1 ;
+        while ( l < r )
+        {
+            while ( l < r && !AlphaNum(s[l]))
+            {
+                l++;
+            }
+            while ( r > l && !AlphaNum(s[r]))
+            {
+                r--;
+            }
+
+            if(char.ToLower(s[l]) != char.ToLower(s[r]))
+            {
+                return false ;
+            }
+            l++; r--;
+        }
+        return true ;
+    }
+
+    public bool AlphaNum(char c )
+    {
+        return ( c >= 'a' && c <= 'z' ||
+                 c >= 'A' && c <= 'Z' ||
+                 c >= '0' && c <= '9');
+    }
+}
