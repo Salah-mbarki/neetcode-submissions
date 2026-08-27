c4c023fefc1496c94ea3339e48954e59f3bdd891
Salah-mbarki <145559443+Salah-mbarki@users.noreply.github.com>
2026-08-28 00:58:32 +0100

    Add: valid-palindrome-ii - submission-20

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-20.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public bool ValidPalindrome(string s) {
+        int l = 0 , r = s.Length-1 ;
+        while ( l < r)
+        {
+            if ( l < r && s[l] != s[r])
+            {
+                string skipL = s.Substring( l , r-l);
+                string skipR = s.Substring(l+1 , r-l);
+                return isPalindrome(skipL) || isPalindrome(skipR);
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+    public bool isPalindrome(string s)
+    {
+        int l = 0 , r = s.Length - 1;
+        while( l < r)
+        {
+            if (s[l] != s[r])
+            {
+                return false ;
+                break;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
